3bd4c9296f16ab1fa8daaa69fb9ddcb06e63cf50
Wes <whicks@redventures.com>
2018-02-22 08:31:46 -0500

    add files from matt's zip

--- main.js
+++ main.js
@@ -1,156 +1,184 @@
-// Get map options:
-var dropdownOptions =  document.getElementsByClassName("dropdown-option");
+/*
+		Marker class
+*/
 
-document.getElementById('search').focus();
-// Change display based on URL
-changeFromURL(dropdownOptions);
+// Establish Marker class
+class Marker {
 
-// Initialize Shared URL
-createSharedURL();
-
-// Add event handlers to dropdown menu
-handleDropdown(dropdownOptions);
-
-
-/* Change the image in the map based on URL params or dropdown selections */
-
-// Change map image source URL
-function changeImgSrc(id) {
-	// Change image
-	var imgSrc = "./img/"+id+".jpg"
-  document.getElementById("mapImage").src = imgSrc;
-
-  // Log
-  console.log("changing image to "+id);
-}
-
-// Change map based on URL parameters
-function changeFromURL(dropdownOptions) {
-	// Grab parameters from the URL, establish document.params as the current location
-	document.params = {};
-	try {
-		document.params = document.location.href.split('?')[1].split('&').reduce(function(obj, param) {
-	    var keyValArr = param.split('=');
-	    obj[keyValArr[0]] = decodeURIComponent(keyValArr[1]);
-	    return obj
-		},{});
-	}
-	catch(err) {
-		console.log("no parameters found");
-	}
-
-	// Pull possible map IDs out of the dropdown HTML
-	var options = Array.prototype.map.call(dropdownOptions, function(el) {
-		return el.id;
-	});
-
-	// Change the image if the URL supplies a map ID
-	if(options.indexOf(document.params.id) !== -1) {
-			changeImgSrc(document.params.id);
-			setMarker(document.params);
-	} else if (document.params.id || document.params.x || document.params.y) {
-		document.params = {};
-		alert("Location not recognized!");
-	}
-
-	// Set the market if the URL supplies a location
-
-}
-
-// Change map based on dropdown selections
-function changeFromDropdown(id) {
-	changeImgSrc(id);
-}
-
-/* Handle map markers & sharing URL */
-
-// Add marker when the map is clicked
-function mapClick(e) {
-	var clickLocation = {"x": e.pageX, "y": e.pageY};
-	// Set a marker at the click location
-  setMarker(clickLocation);
-  // Update the sharing URL with the click location
-  updateSharedURL("upsert", clickLocation);
-}
-
-function setMarker(clickLocation) {
-	with(document.getElementById('marker'))
-  {
-    style.left = clickLocation.x - 70;
-    style.top = clickLocation.y - 300;
-    style.display = 'block';
-  }
-}
-
-function clearMarker() {
-	with(document.getElementById('marker'))
-	{
-		style.display = 'none';
-	}
-}
-
-function createSharedURL() {
-		// Iterate through the existing parameters, append them to a shareable URL
-		var shareUrl = Object.keys(document.params).reduce(function(url, parameter) {
-				return url + (url.indexOf('?') === -1 ? '?' : '&') + parameter + "=" + document.params[parameter]
-		},document.location.href.split('?')[0]);
-		document.getElementById('share').value = shareUrl;
-		try {
-			window.history.pushState('','','?'+shareUrl.split('?')[1]);
-		} catch(e) {
-			console.log(e);
+		constructor(htmlId) {
+				// Define the current location
+				this.floor = "rv1_1";
+				this.x = null;
+				this.y = null;
+				this.room = null;
+				// Define acceptable floors
+				this.floorList = ['rv1_1', 'rv1_2', 'rv2_1', 'rv2_2', 'rv3_0b', 'rv3_0g', 'rv3_1', 'rv3_2', 'rv3_3', 'rv4_1', 'rv4_2', 'rv5_1', 'rv5_2', 'rv5_3', 'rv5_4', 'rv5_5', 'rv5_6', 'rv6_1', 'rv6_2'];
+				// Find the Marker Image
+				this.htmlId = htmlId;
+				// Tell GTM about it
+				dataLayer.push({
+						'event': 'markerEstablished',
+						'marker': {
+								'floor': this.floor,
+								'x': this.x,
+								'y': this.y,
+								'room': this.room
+						}
+				});
 		}
-		dataLayer.push({'event': 'locationUpdated', 'location': document.params});
-}
 
-function updateSharedURL(type, locationObj) {
-		// Edit the document.params object to store current location
-		if (type === 'clear') {
-				document.params = locationObj;
-		} else if (type === 'upsert') {
-				Object.keys(locationObj).forEach(function(key) {
-					document.params[key] = locationObj[key];
-				})
+		updateLocation(location) {
+				// Update the marker object location
+				this.floor = location.floor || this.floor;
+				this.x = location.x || null;
+				this.y = location.y || null;
+				this.room = location.room || null;
+				// Update the URL for sharing
+				var search = '?floor='+this.floor+(this.x&&this.y?'&x='+this.x+'&y='+this.y+(this.room?'&room='+this.room:''):'')
+				window.history.pushState('','',search);
 		}
-		createSharedURL();
+
+		clearImage() {
+				var el = document.getElementById(this.htmlId);
+				el.style.display = 'none';
+		}
+
+		setImage(x, y) {
+				var el = document.getElementById(this.htmlId);
+				el.style.display = 'block';
+				el.style.left = x;
+				el.style.top = y;
+		}
+
+		update(updateType, markerInput) {
+				var markerUpdate = {};
+
+				// Check for valid floor
+				if (this.floorList.indexOf(markerInput.floor) !== -1) {
+
+						// Keep track of the floor to update our Marker object
+						markerUpdate.floor = markerInput.floor;
+
+						// Update the map image, if necessary
+						if(markerInput.floor !== document.marker.floor) {
+								updateMapImage(markerInput.floor);
+						}
+
+						// If either x or y is invalid, clear the marker. Otherwise, set the marker
+						if (isNaN(parseInt(markerInput.x)) || isNaN(parseInt(markerInput.y))) {
+
+								// Clear the marker, since an update was attempted
+								this.clearImage();
+
+								console.error('Location Not Recognized: '+JSON.stringify(markerInput))
+						} else {
+
+								// Set the marker in the proper location
+								this.setImage(markerInput.x, markerInput.y);
+
+								// Keep track of the location to update our Marker object
+								markerUpdate.x = markerInput.x;
+								markerUpdate.y = markerInput.y;
+						}
+
+						// If we have a room name specified, add that to our update
+						markerUpdate.room = markerInput.room || null;
+
+						// Make the update
+						this.updateLocation(markerUpdate);
+
+						dataLayer.push({
+								'event': 'markerUpdated',
+								'updateType': updateType,
+								'marker': markerUpdate,
+								'input': markerInput,
+								'xy-success': (markerInput.x === markerUpdate.x)
+						});
+
+				} else if (markerInput.floor) {
+						// If there is a floor specified but it is invalid, say so
+						console.error('Invalid Floor Selected: '+JSON.stringify(markerInput));
+
+						dataLayer.push({
+								'event': 'markerUpdateFailed',
+								'updateType': updateType,
+								'input': markerInput
+						});
+				} else {
+						// If no floor is specified, do nothing
+						console.log('No update needed from '+updateType);
+				}
+
+		}
 }
 
-/* Handle dropdown menu functionality  */
+/*
+		Page Load Functions
+*/
 
-// When the user clicks on the button, toggle between hiding and showing the dropdown content 
-function toggleDropdown() {
-		// Open/Close Dropdown
-    document.getElementById("myDropdown").classList.toggle("show");
+// Establish marker instance
+document.marker = new Marker('marker');
 
+updateFromUrl();
+
+
+/*
+		Marker Update Functions
+*/
+
+function updateFromUrl() {
+		if (document.location.search.length !== 0) {
+				// Take location parameters from URL
+				var marker = document.location.search.replace('?','').split('&').reduce(function(marker,param) {
+						var keyVal = param.split('=');
+						marker[keyVal[0]] = decodeURIComponent(keyVal[1]);
+						return marker;
+				},{});
+
+				console.log(marker);
+
+				// Update the current marker
+				document.marker.update('url', marker);
+		}
 }
 
-// On page-load, add all necessary event listeners to the dropdown menu
-function handleDropdown(dropdownOptions) {
+function updateFromMapClick(click) {
+		// Take location parameters from Click
+		var marker = {
+				'floor': document.marker.floor,
+				'x': click.pageX,
+				'y': click.pageY
+		};
 
-	// Close the dropdown menu if the user clicks outside of it
-	window.onclick = function(event) {
-	  if (!event.target.matches('.dropbtn')) {
-
-	    var dropdowns = document.getElementsByClassName("dropdown-content");
-	    var i;
-	    for (i = 0; i < dropdowns.length; i++) {
-	      var openDropdown = dropdowns[i];
-	      if (openDropdown.classList.contains('show')) {
-	        openDropdown.classList.remove('show');
-	      }
-	    }
-	  }
-	}
-
-	// Handle dropdown changes with event listener
-	Array.prototype.forEach.call(dropdownOptions,function(el) {
-		el.addEventListener('click', function() {
-			changeFromDropdown(el.id);
-			clearMarker();
-			updateSharedURL("clear", {"id": el.id});
-	  }, false);
-	});
+		// Update the current marker
+		document.marker.update('click', marker);
 }
 
+function updateFromSearch(result) {
+		// Take location parameters from Search Result
+		var marker = {};
 
+		// Update the current marker
+		document.marker.update('search', marker);
+}
 
+function updateFromFloorSelection(click) {
+		// Take location parameters from Floor Selection
+		var marker = {};
+
+		// Update the current marker
+		document.marker.update('floor select', marker);
+}
+
+/*
+		UI Functions
+*/
+
+function updateMapImage(floor) {
+		// Change image
+		var imgSrc = "./img/"+floor+".jpg"
+	  document.getElementById("mapImage").src = imgSrc;
+
+	  // Log
+	  console.log("changing image to "+floor);
+}
